7f81b101321a8beb7eccbc20f52cec0081622763
tusharr81 <choudhary.tushar2005@gmail.com>
2024-03-15 18:45:48 +0530

    fixed curly braces

--- Smart Contracts/zombiefeeding.sol
+++ Smart Contracts/zombiefeeding.sol
@@ -3,11 +3,11 @@ import "./Zombiefactory.sol";
 
 contract Zombiefeeding is Zombiefactory{
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    Zombie storage myZombie = zombies[_zombieId];
-
-    //Dna of new zombie after feeding
-    _targetDna = _targetDna % dnaModulus;
-    uint newDna =  (myZombie.dna + _targetDna) / 2;
-    _createZombie("NoName", newDna);
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        //Dna of new zombie after feeding
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna =  (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+        }
 }

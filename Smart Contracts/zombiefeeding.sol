6f39931220b2338b09eb27e2f16f449fa0d3901e
tusharr81 <choudhary.tushar2005@gmail.com>
2024-03-15 18:30:42 +0530

    Adding a function feedAndMultiply

--- Smart Contracts/zombiefeeding.sol
+++ Smart Contracts/zombiefeeding.sol
@@ -2,5 +2,7 @@ pragma solidity 0.8.24;
 import "./Zombiefactory.sol";
 
 contract Zombiefeeding is Zombiefactory{
-
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
 }
